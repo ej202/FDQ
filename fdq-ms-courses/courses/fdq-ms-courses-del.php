78e36ad3833c2d9b81be63a904ecf508a4e14855
ej202 <64786068+ej202@users.noreply.github.com>
2023-09-11 14:45:02 -0600

    Update fdq-ms-courses-del.php

--- fdq-ms-courses/courses/fdq-ms-courses-del.php
+++ fdq-ms-courses/courses/fdq-ms-courses-del.php
@@ -1,1 +1,33 @@
-hello this is fdq-ms-courses-del.php
+<?php
+// Ensure this file is being accessed from a WordPress context
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+// Check if courseId is set in the GET request
+if (isset($_GET['courseId']) && !empty($_GET['courseId'])) {
+    global $wpdb; // WordPress database object
+
+    $courseId = intval($_GET['courseId']); // Sanitize input
+
+    // SQL DELETE query
+    $result = $wpdb->delete(
+        'wpou_mscm_course', // Table name
+        array('courseId' => $courseId), // WHERE clause
+        array('%d') // Data format
+    );
+
+    // Check result and provide feedback
+    if ($result !== false) {
+        // Redirect back with a success message (you can customize the message as needed)
+        wp_redirect(admin_url('admin.php?page=fdq-ms-courses.php&message=success'));
+    } else {
+        // Redirect back with an error message (you can customize the message as needed)
+        wp_redirect(admin_url('admin.php?page=fdq-ms-courses.php&message=error'));
+    }
+} else {
+    // Redirect back with an error message if courseId is not provided
+    wp_redirect(admin_url('admin.php?page=fdq-ms-courses.php&message=missing_id'));
+}
+exit;
+?>
